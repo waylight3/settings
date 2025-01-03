3b68d752906f525f042f237f07d2945bfce0cc30
Dohyeon Lee <waylight3@snu.ac.kr>
2025-01-03 12:34:50 +0900

    remove pyserini handling

--- envset.sh
+++ envset.sh
@@ -10,9 +10,3 @@ cd ..
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/waylight3/settings.git
 cp settings/.vimrc ~/.vimrc
-
-# install pyserini
-pip install --upgrade pybind11
-pip install --verbose  'nmslib @ git+https://github.com/nmslib/nmslib.git#egg=nmslib&subdirectory=python_bindings'
-pip install pyserini
-
